a16b45dd703ca61bc1bdb11ae00048c269c4d1d2
DanilloRamos <danillosi__@live.com>
2025-07-11 16:32:20 +0000

    implementação de algumas funcionalidades da calculadora imc com react

--- Projetos/Calculadora IMC com React/imc-calc/src/components/ImcCalc.jsx
+++ Projetos/Calculadora IMC com React/imc-calc/src/components/ImcCalc.jsx
@@ -1,7 +1,25 @@
 import './ImcCalc.css'
 import Button from './Button'
+import { useState } from 'react'
 
 const ImcCalc = () => {
+    const [height, setHeight] = useState('')
+    const [weight, setWeight] = useState('')
+
+    const clearForm = (e) => {
+        e.preventDefault()
+
+        setHeight('')
+        setWeight('')
+
+    }
+
+    
+
+    const handleHeightChange = (e) => {
+        setHeight(e.target.value)
+    }
+    
     return (
         <div id='calc-container'>
             <h2>Calculadora de IMC</h2>
@@ -9,16 +27,16 @@ const ImcCalc = () => {
                 <div className='form-inputs'>
                     <div className='form-control'>
                         <label htmlFor='height'>Altura:</label>
-                        <input type='text' name='height' id='height' placeholder='Exemplo: 1,75'/>
+                        <input type='text' name='height' id='height' placeholder='Exemplo: 1,75' onChange={(e) => handleHeightChange(e)} value={height}/>
                     </div>
                     <div className='form-control'>
                         <label htmlFor='weight'>Peso:</label>
-                        <input type='text' name='weight' id='weight' placeholder='Exemplo: 70,5'/>
+                        <input type='text' name='weight' id='weight' placeholder='Exemplo: 70,5' onChange={(e) => setWeight(e.target.value)} value={weight}/>
                     </div>
                 </div>
                 <div className='action-control'>
                     <Button id='calc-btn' text='Calcular'/>
-                    <Button id='clear-btn' text='Limpar' />
+                    <Button id='clear-btn' text='Limpar' action={clearForm}/>
                 </div>
             </form>
         </div>
