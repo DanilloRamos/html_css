45d082c3c072f95578d356a7c509f139df86fa49
DanilloRamos <danillosi__@live.com>
2025-07-28 19:13:54 +0000

    projeto contador regressivo

---
+++ Projetos/Contador Regressivo/countdown/src/App.jsx
@@ -0,0 +1,25 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+import NewYear from './assets/newyear.jpg'
+import { Outlet } from 'react-router-dom'
+import { useContext } from 'react'
+import { CountdownContext } from './context/CountdownContext'
+import './App.css'
+
+function App() {
+  const {event} = useContext(CountdownContext)
+  let eventImage = null
+
+  if(event) eventImage = event.image
+
+  return (
+    <div className="App" style={eventImage ? {backgroundImage: `url(${eventImage})`} : {backgroundImage: `url(${NewYear})`}}>
+      <div className="container">
+        
+        <Outlet/>
+      </div>
+    </div>
+  )
+}
+
+export default App
